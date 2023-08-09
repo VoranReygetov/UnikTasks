f6c71918213147397b208bbeb1b57f8ad2b580a3
VoranReygetov <107549898+VoranReygetov@users.noreply.github.com>
2023-08-09 19:38:03 +0300

    Add files via upload

---
+++ Savchenko_Volodymyr_3-main/Savchenko_Volodymyr_3-main/Прога лаба 3.2/Прога лаба 3.2/Bus.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Прога_лаба_3._2
+{
+    class Bus
+    {
+        public int settles { get; set; }
+        public string number { get; set; }
+        public int fuel { get; set; }
+        
+        public Bus()
+        {
+            settles = 30;
+            number = "0000";
+            fuel = 24;
+        }
+        public Bus(int settles, string number, int fuel)
+        {
+            this.settles = settles;
+            this.number = number;
+            this.fuel = fuel;
+        }
+
+    }
+    class BigBus : Bus
+    {
+        public BigBus()
+        {
+            settles = 54;
+            number = "1111";
+            fuel = 34;
+        }
+        public BigBus(int settles, string number, int fuel)
+        {
+            this.settles = settles;
+            this.number = number;
+            this.fuel = fuel;
+        }
+    }
+    class MidBus : Bus
+    {
+        public MidBus()
+        {
+            settles = 35;
+            number = "2222";
+            fuel = 27;
+        }
+        public MidBus(int settles, string number, int fuel)
+        {
+            this.settles = settles;
+            this.number = number;
+            this.fuel = fuel;
+        }
+    }
+    class SmallBus : Bus
+    {
+        public SmallBus()
+        {
+            settles = 22;
+            number = "3333";
+            fuel = 23;
+        }
+        public SmallBus(int settles, string number, int fuel)
+        {
+            this.settles = settles;
+            this.number = number;
+            this.fuel = fuel;
+        }
+
+    }
+}
---
+++ Savchenko_Volodymyr_3-main/Savchenko_Volodymyr_3-main/Прога лаба 3.2/Прога лаба 3.2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Прога_лаба_3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Station station = new Station();
+            Bus bus = new Bus();
+            BigBus big = new BigBus();
+            MidBus mid = new MidBus();
+            SmallBus small = new SmallBus();
+
+            station.AddBus(mid);
+            station.AddBus(small);
+            station.AddBus(big);
+
+            Console.WriteLine("Автобусна станцiя Київ вiтає вас.");
+            Console.WriteLine("Зараз в нас є такi автобуси:");
+            foreach (var buss in station.Busses)
+            {
+                Console.WriteLine($"Це :{buss.number}, витрати топлива:{buss.fuel}, кiлькiсть мiсць:{buss.settles}");
+            }
+            Console.WriteLine("\nЯкщо їх вiдсортувати за кiлькiстю сидiнь, то: ");
+
+            station.SortbySettles();
+
+            foreach (var buss in station.Busses)
+            {
+                Console.WriteLine($"Це :{buss.number}, витрати топлива:{buss.fuel}, кiлькiсть мiсць:{buss.settles}");
+            }
+            Console.WriteLine("\nТакож, можна знайти автобус за його номером. Впишiть номер:");
+
+            station.SearchNum(Console.ReadLine());
+
+            Console.WriteLine("\nТа за витратами пального:");
+
+            station.SearchFuel(int.Parse(Console.ReadLine()));
+
+            Console.Read();
+        }
+
+    }
+}
